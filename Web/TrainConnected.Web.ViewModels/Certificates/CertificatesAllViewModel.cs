3bd26a2cdfff908ee5591dbca76d077c5a0c1b7a
StefanLB <s_baliev@yahoo.com>
2019-07-16 10:57:15 +0300

    Fixed datetime to date formatting, replaced htmlhelpers with taghelpers for Certificates/All view

--- Web/TrainConnected.Web.ViewModels/Certificates/CertificatesAllViewModel.cs
+++ Web/TrainConnected.Web.ViewModels/Certificates/CertificatesAllViewModel.cs
@@ -1,7 +1,8 @@
 ﻿namespace TrainConnected.Web.ViewModels.Certificates
 {
     using System;
-
+    using System.ComponentModel.DataAnnotations;
+    using System.Globalization;
     using TrainConnected.Data.Models;
     using TrainConnected.Data.Models.Enums;
     using TrainConnected.Services.Mapping;
@@ -12,12 +13,16 @@
 
         public WorkoutActivity Activity { get; set; }
 
+        [Display(Name = "Issued By")]
         public string IssuedBy { get; set; }
 
+        [Display(Name = "Issued To")]
         public string IssuedTo { get; set; }
 
-        public DateTime IssuedOn { get; set; }
+        [Display(Name = "Issued On")]
+        public DateTime IssuedOn { get ; set; }
 
+        [Display(Name = "Expires On")]
         public DateTime? ExpiresOn { get; set; }
 
         public string Description { get; set; }
