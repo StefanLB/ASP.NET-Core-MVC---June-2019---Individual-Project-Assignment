5044f92230f802bc3901c4c08ac6ac2f6e1c963d
StefanLB <s_baliev@yahoo.com>
2019-07-20 19:47:16 +0300

    Implemented Cancel and list all functionality for Bookings

---
+++ Web/TrainConnected.Web.ViewModels/Bookings/BookingsAllViewModel.cs
@@ -0,0 +1,38 @@
+﻿namespace TrainConnected.Web.ViewModels.Bookings
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using TrainConnected.Data.Models;
+    using TrainConnected.Services.Mapping;
+
+    public class BookingsAllViewModel : IMapFrom<Booking>
+    {
+        public string Id { get; set; }
+
+        [Display(Name = "Payment Method")]
+        public string PaymentMethod { get; set; }
+
+        public decimal Price { get; set; }
+
+        [Display(Name = "Created On")]
+        public DateTime CreatedOn { get; set; }
+
+        [Display(Name = "Activity")]
+        public string WorkoutActivityName { get; set; }
+
+        [Display(Name = "Coach")]
+        public string WorkoutCoachUserName { get; set; }
+
+        [Display(Name = "Time")]
+        public DateTime WorkoutTime { get; set; }
+
+        [Display(Name = "Location")]
+        public string WorkoutLocation { get; set; }
+
+        [Display(Name = "Duration")]
+        public int WorkoutDuration { get; set; }
+
+        [Display(Name = "Notes")]
+        public string WorkoutNotes { get; set; }
+    }
+}
