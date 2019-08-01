e6eb20f3f9956e9e2cfabae1f3dc3e26feb04d93
StefanLB <s_baliev@yahoo.com>
2019-08-01 22:00:28 +0300

    Added some AchievementService tests. Not fully covered

--- Tests/TrainConnected.Services.Data.Tests/AchievementsServiceTests.cs
+++ Tests/TrainConnected.Services.Data.Tests/AchievementsServiceTests.cs
@@ -1,6 +1,8 @@
 ﻿namespace TrainConnected.Services.Data.Tests
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Reflection;
     using System.Threading.Tasks;
     using Microsoft.EntityFrameworkCore;
@@ -11,6 +13,7 @@
     using TrainConnected.Services.Mapping;
     using TrainConnected.Web.InputModels.WorkoutActivities;
     using TrainConnected.Web.ViewModels;
+    using TrainConnected.Web.ViewModels.Achievements;
     using Xunit;
 
     public class AchievementsServiceTests
@@ -45,7 +48,167 @@
         [Fact]
         public async Task TestGetAllAsync_WithTestData_ShouldReturnAllAchievements()
         {
-            var result = Achievement
+            var userId = "testUserId";
+            var achievementsDictionary = this.FillAllAchievements();
+
+            foreach (var achievement in achievementsDictionary)
+            {
+                await this.achievementsRepository.AddAsync(new Achievement()
+                {
+                    Name = achievement.Key,
+                    Description = achievement.Value,
+                    TrainConnectedUserId = userId,
+                });
+
+            }
+
+            await this.achievementsRepository.SaveChangesAsync();
+
+            var expectedAchievementsCount = achievementsDictionary.Count;
+
+            var actualAchievements = await this.achievementsService.GetAllAsync(userId);
+            var actualAchievementsCount = actualAchievements.Count();
+
+            Assert.Equal(expectedAchievementsCount, actualAchievementsCount);
+        }
+
+        [Fact]
+        public async Task TestGetAllAsync_WithNoData_ShouldReturnNoAchievements()
+        {
+            var userId = "testUserId";
+
+            IEnumerable<AchievementsAllViewModel> expectedResult = new List<AchievementsAllViewModel>();
+
+            var actualResult = await this.achievementsService.GetAllAsync(userId);
+
+            Assert.Equal(expectedResult.Count(), actualResult.Count());
+        }
+
+        [Fact]
+        public async Task TestGetDetailsAsync_WithCorrectData_ShouldReturnAchievement()
+        {
+            var userId = "testUserId";
+            var achievementId = "testAchievementId";
+
+            var testAchievement = new Achievement()
+            {
+                Id = achievementId,
+                Name = ServiceConstants.Achievement.AdventurerAchievementName,
+                Description = ServiceConstants.Achievement.AdventurerAchievementDescription,
+                TrainConnectedUserId = userId,
+            };
+
+            await this.achievementsRepository.AddAsync(testAchievement);
+            await this.achievementsRepository.SaveChangesAsync();
+
+            var expectedAchievement = testAchievement;
+            var actualAchievement = await this.achievementsService.GetDetailsAsync(achievementId, userId);
+
+            Assert.Equal(expectedAchievement.Name, actualAchievement.Name);
+            Assert.Equal(expectedAchievement.Description, actualAchievement.Description);
+        }
+
+        [Fact]
+        public async Task TestGetDetailsAsync_WithIncorrectAchievementId_ShouldThrowNullRefEx()
+        {
+            var userId = "testUserId";
+            var achievementId = "testAchievementId";
+            var incorrectAchievementId = "incorrectTestAchievementId";
+
+            var testAchievement = new Achievement()
+            {
+                Id = achievementId,
+                Name = ServiceConstants.Achievement.AdventurerAchievementName,
+                Description = ServiceConstants.Achievement.AdventurerAchievementDescription,
+                TrainConnectedUserId = userId,
+            };
+
+            await this.achievementsRepository.AddAsync(testAchievement);
+            await this.achievementsRepository.SaveChangesAsync();
+
+            await Assert.ThrowsAsync<NullReferenceException>(async () => await this.achievementsService.GetDetailsAsync(incorrectAchievementId, userId));
+        }
+
+        [Fact]
+        public async Task TestGetDetailsAsync_WithIncorrectUserId_ShouldThrowArgumentEx()
+        {
+            var userId = "testUserId";
+            var incorrectUserId = "incorrectUserId";
+            var achievementId = "testAchievementId";
+
+            var testAchievement = new Achievement()
+            {
+                Id = achievementId,
+                Name = ServiceConstants.Achievement.AdventurerAchievementName,
+                Description = ServiceConstants.Achievement.AdventurerAchievementDescription,
+                TrainConnectedUserId = userId,
+            };
+
+            await this.achievementsRepository.AddAsync(testAchievement);
+            await this.achievementsRepository.SaveChangesAsync();
+
+            await Assert.ThrowsAsync<ArgumentException>(async () => await this.achievementsService.GetDetailsAsync(achievementId, incorrectUserId));
+        }
+
+        [Fact]
+        public async Task TestCreateAchievementAsync_WithCorrectData_ShouldCreateAchievement()
+        {
+            var testAchievement = new Achievement
+            {
+                Name = "Testname",
+                Description = "TestDecription",
+                AchievedOn = DateTime.UtcNow,
+                TrainConnectedUserId = "TestUserId",
+            };
+
+            await this.achievementsService.CreateAchievementAsync(
+                testAchievement.Name,
+                testAchievement.TrainConnectedUserId,
+                testAchievement.Description,
+                testAchievement.AchievedOn);
+
+            var expectedAchievement = testAchievement;
+            var actualAchievement = await this.achievementsRepository.All()
+                .FirstOrDefaultAsync();
+
+            Assert.Equal(expectedAchievement.Name, actualAchievement.Name);
+            Assert.Equal(expectedAchievement.Description, actualAchievement.Description);
+            Assert.Equal(expectedAchievement.AchievedOn, actualAchievement.AchievedOn);
+            Assert.Equal(expectedAchievement.TrainConnectedUserId, actualAchievement.TrainConnectedUserId);
+        }
+
+        [Fact]
+        public async Task TestCheckForAchievementsAsync_WithNoFullfilledConditions_ShouldCreateNoAchievements()
+        {
+            string testUserId = "TestUserId";
+
+            await this.achievementsService.CheckForAchievementsAsync(testUserId);
+
+            var actualResult = await this.achievementsRepository.All()
+                .ToArrayAsync();
+
+            Assert.Empty(actualResult);
+        }
+
+        private Dictionary<string, string> FillAllAchievements()
+        {
+            var achievementsDictionary = new Dictionary<string, string>
+            {
+                { ServiceConstants.Achievement.AdventurerAchievementName, ServiceConstants.Achievement.AdventurerAchievementDescription },
+                { ServiceConstants.Achievement.BigSpenderAchievementName, ServiceConstants.Achievement.BigSpenderAchievementDescription },
+                { ServiceConstants.Achievement.CantGetEnoughAchievementName, ServiceConstants.Achievement.CantGetEnoughAchievementName },
+                { ServiceConstants.Achievement.CuttingItCloseAchievementName, ServiceConstants.Achievement.CuttingItCloseAchievementDescription },
+                { ServiceConstants.Achievement.DoubleTroubleAchievementName, ServiceConstants.Achievement.DoubleTroubleAchievementDescription },
+                { ServiceConstants.Achievement.EarlyBirdAchievementName, ServiceConstants.Achievement.EarlyBirdAchievementDescription },
+                { ServiceConstants.Achievement.FirstResponderAchievementName, ServiceConstants.Achievement.FirstResponderAchievementDescription },
+                { ServiceConstants.Achievement.FirstWorkoutAchievementName, ServiceConstants.Achievement.FirstWorkoutAchievementDescription },
+                { ServiceConstants.Achievement.GettingStartedAchievementName, ServiceConstants.Achievement.GettingStartedAchievementDescription },
+                { ServiceConstants.Achievement.MedicAchievementName, ServiceConstants.Achievement.MedicAchievementDescription },
+                { ServiceConstants.Achievement.NightOwlAchievementName, ServiceConstants.Achievement.NightOwlAchievementDescription },
+                { ServiceConstants.Achievement.VeteranAchievementName, ServiceConstants.Achievement.VeteranAchievementDescription },
+            };
+
+            return achievementsDictionary;
         }
     }
 }
---
+++ Tests/TrainConnected.Services.Data.Tests/BookingsServiceTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainConnected.Services.Data.Tests
+{
+    class BookingsServiceTests
+    {
+    }
+}
